ee53f311a3296ddc47c49c48424243ccc90c9963
yuyutti <yuyutti@yuyutti.com>
2025-03-07 19:57:47 +0900

    chore: Change time input field type from tel to text for improved user experience

--- public/function.js
+++ public/function.js
@@ -47,7 +47,7 @@ function createTimeInputField(containerId, dateInputId, timeInputId, includeDate
         <div class="input-group">
             ${includeDate ? `<input type="date" class="form-control" id="${dateInputId}" required>` : ''}
             <input type="tel" class="form-control" id="${timeInputId}" 
-                placeholder="HH:mm" pattern="[0-9]{2}:[0-9]{2}" tabindex="4" autocomplete="off" required>
+                placeholder="HH:mm" tabindex="4" autocomplete="off" required>
             <div id="timeDropdown" class="time-dropdown"></div>
             <datalist id="timeList">
                 ${
